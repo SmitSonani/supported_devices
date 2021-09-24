aa9ae0b414cc5879c3349a63085f3f9fc6790dd0
Smit Sonani <sonanismit@gmail.com>
2021-09-24 11:14:19 +0530

    Feature: common platform interface (#1)
    * updated .gitignore
    
    * feature: common platform interface for supported_devices plugin

---
+++ supported_devices_platform_interface/lib/src/method_channel_supported_devices.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/services.dart';
+
+import 'supported_devices_platform_interface.dart';
+
+class MethodChannelSupportedDevices extends SupportedDevicesPlatform {
+  final _methodChannel = const MethodChannel('supported_devices');
+  final _eventChannel = const EventChannel('supported_devices/events');
+
+  @override
+  Future<String?> getRandomSupportedDevice() =>
+      _methodChannel.invokeMethod<String>('getRandom');
+
+  @override
+  Future<void> toggleStreamingSupportedDevices(bool start) =>
+      _methodChannel.invokeMethod<void>('toggleStreaming');
+
+  @override
+  Stream<String> supportedDevicesStream() => _eventChannel
+      .receiveBroadcastStream()
+      .map(_mapEvent)
+      .where((event) => event != null)
+      .map((event) => event!);
+
+  String? _mapEvent(dynamic event) {
+    final eventName = event['name'] as String;
+    if (eventName == 'supportedDevice') {
+      final device = event['data'] as String;
+      return device;
+    }
+  }
+}
---
+++ supported_devices_platform_interface/lib/supported_devices_platform_interface.dart
@@ -0,0 +1,1 @@
+export 'src/supported_devices_platform_interface.dart';
